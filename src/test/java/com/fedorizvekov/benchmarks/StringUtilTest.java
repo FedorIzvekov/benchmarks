31b273119226bd6fcae94405543e3fcef17c8cec
FedorIzvekov <fedor.izvekov@gmail.com>
2019-11-30 11:54:18 +0300

    benchmarks: updated dependencies

--- src/test/java/com/fedorizvekov/benchmarks/StringUtilTest.java
+++ src/test/java/com/fedorizvekov/benchmarks/StringUtilTest.java
@@ -2,15 +2,17 @@ package com.fedorizvekov.benchmarks;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
-public class StringUtilTest {
+@ExtendWith(MockitoExtension.class)
+class StringUtilTest {
 
     @Test
-    public void should_generate_string_of_length_10() {
+    @DisplayName("Should generate string of length 10")
+    void should_generate_string_of_length_10() {
         String result = StringUtil.generate(10);
         assertThat(result).hasSize(10);
         assertThat(result).matches("[A-Za-z]+");
@@ -18,7 +20,8 @@ public class StringUtilTest {
 
 
     @Test
-    public void should_generate_string_of_length_100() {
+    @DisplayName("Should generate string of length 100")
+    void should_generate_string_of_length_100() {
         String result = StringUtil.generate(100);
         assertThat(result).hasSize(100);
         assertThat(result).matches("[A-Za-z]+");
@@ -26,7 +29,8 @@ public class StringUtilTest {
 
 
     @Test
-    public void should_repeat_1_times() {
+    @DisplayName("Should repeat 1 times")
+    void should_repeat_1_times() {
         String result = StringUtil.repeat("Test", 1);
         assertThat(result).hasSize(4);
         assertThat(result).isEqualTo("Test");
@@ -34,7 +38,8 @@ public class StringUtilTest {
 
 
     @Test
-    public void should_repeat_5_times() {
+    @DisplayName("Should repeat 5 times")
+    void should_repeat_5_times() {
         String result = StringUtil.repeat("Test", 5);
         assertThat(result).hasSize(20);
         assertThat(result).isEqualTo("TestTestTestTestTest");
